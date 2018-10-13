404ef9bb68bc760d9a5dec450e400dc7bd04d2a0
Rehan Vipin <43005490+rehanvipin@users.noreply.github.com>
2018-10-13 22:26:03 +0530

    Create main_game.py

---
+++ main_game.py
@@ -0,0 +1,19 @@
+import player
+jas = player.Player()
+
+def talk_to_bot():
+  pass # to talk to the bot 
+
+while playing:
+  pass # the game loop where the player will be interacting with the console
+  '''
+  .
+  .
+  .
+  '''
+  if not keys:
+    go_back() # example of decision control statements
+    
+   
+with open('records','a') as record:
+  record.write(plas.progress)
