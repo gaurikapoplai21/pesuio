7272d0a28d2d6db325e1d93f7d59cffadf882bd7
Rehan Vipin <43005490+rehanvipin@users.noreply.github.com>
2018-11-11 13:48:49 +0530

    Update player.py

--- player.py
+++ player.py
@@ -3,8 +3,12 @@ class Player():
         self.dead = False
         self.Virtue = 0
         self.bag = []
+        self.position = "home"
     def move(self,dest):
         self.positon = dest
     def talk(self):     # this needs to be filled up with the valid command and so do the previous two functions
         return "continue"
+    def pick(self,obj):
+        if obj not in self.bag:
+            bag.append(obj)
     
