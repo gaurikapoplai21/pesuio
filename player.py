4ea0b0f1c2efb5e584d764f41729de1f25c4cc3a
Rehan Vipin <43005490+rehanvipin@users.noreply.github.com>
2018-11-11 13:51:09 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,11 +1,15 @@
 class Player():
+    valid_locations = []
     def __init__(self):
         self.dead = False
         self.Virtue = 0
         self.bag = []
         self.position = "home"
     def move(self,dest):
-        self.positon = dest
+        if dest in self.valid_locations:
+            self.position = dest
+           else:
+            print("You cannot go there")
     def talk(self):     # this needs to be filled up with the valid command and so do the previous two functions
         return "continue"
     def pick(self,obj):
